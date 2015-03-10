023ae5bbce309d1468acb27b36f34d6dcf47551d
Robert Talamantez <robert.m.talamantez@gmail.com>
2015-03-10 11:52:01 -0700

    building user account creation tests

--- spec/api/auth_spec.js
+++ spec/api/auth_spec.js
@@ -6,10 +6,33 @@ var url = 'https://dev02.canopy.link/api/';
 // Allow self-signed SSL certificates
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
+// Initialize 'New User' credentials
+
+var newUserUN = 'newuser2';
+var newUserEmail = 'newuser2@user.user';
+var newUserPW = 'newuser2';
+
+// Expect 'create_account' to return 200 if username and
+// email are unique
+
 frisby.create('POST account create')
   .post( url + 'create_account',
-    { "username" : "newuser", "email" : "new@user.user",  "password" : "newuser" },
+    { "username" : newUserUN, "email" : newUserEmail,  "password" : newUserPW },
     { json: true },
     { headers: { "Content-Type":"application/json"}})
   .expectStatus(200)
+  .expectHeaderContains('content-type', 'application/json')
   .toss();
+
+// Expect 'create_account' to fail with 400 if username and email
+// already exists
+
+frisby.create('POST account create')
+  .post( url + 'create_account',
+    { "username" : newUserUN, "email" : newUserEmail,  "password" : newUserPW },
+    { json: true },
+    { headers: { "Content-Type":"application/json"}})
+  .expectStatus(400)
+  .expectHeaderContains('content-type', 'application/json')
+  .toss();
+
