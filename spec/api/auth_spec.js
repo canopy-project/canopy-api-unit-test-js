4b2b39e858f0c69b36211beb999d1ff8e37dcd15
Robert Talamantez <robert.m.talamantez@gmail.com>
2015-03-10 11:35:45 -0700

    Added support for self-signed certificates

--- spec/api/auth_spec.js
+++ spec/api/auth_spec.js
@@ -1,8 +1,13 @@
 var frisby = require('frisby');
-var host = 'https://dev02.canopy.link/api/';
+
+// 
+var url = 'https://dev02.canopy.link/api/';
+
+// Allow self-signed SSL certificates
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 frisby.create('POST account create')
-  .post(host + 'create_account',
+  .post( url + 'create_account',
     { "username" : "newuser", "email" : "new@user.user",  "password" : "newuser" },
     { json: true },
     { headers: { "Content-Type":"application/json"}})
